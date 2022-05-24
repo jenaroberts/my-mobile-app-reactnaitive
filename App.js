afffe7f42a1960726e5ecc26a6d674e64af9a474
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-05-24 15:54:22 -0400

    mobile-app-adding-three-buttons

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Counter from "./src/components/Counter";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Button title="Press Me!" />
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 38,
+    color: "#fff",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffaa00",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.counterContainer}>
+      <Text style={styles.counterText}> You clicked me {count} times.</Text>
+      <Button title="+" onPress={() => setCount(count + 1)} />
+      <Button title="Reset" onPress={() => setCount(0)} />
+      <Button title="-" onPress={() => setCount(count - 1)} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  counterContainer: {
+    width: "80vw",
+    margin: 14,
+    padding: 14,
+    backgroundColor: "beige",
+  },
+  counterText: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+});
